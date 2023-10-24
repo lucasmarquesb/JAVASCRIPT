65f3ec7c688962b7650ad4f6116273584053bc51
lucasmarquesb <lucasm.marques100@gmail.com>
2023-10-24 11:02:56 -0300

    exercícios novos em js

---
+++ aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}Km/h `)
+if (vel > 60) { //é uma condição simples!
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre com sinto de segurança! `)
+
