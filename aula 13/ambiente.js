3148b606541a6616eec10ae4728afe5b1e29e81b
lucasmarquesb <lucasm.marques100@gmail.com>
2023-11-09 09:55:43 -0300

    novos ex

---
+++ aula 13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ //é a mesma coisa que c = c + 1
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
