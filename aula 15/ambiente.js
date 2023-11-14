ab0a8b7a5bd07a7c2a6572af386a5da2a4513431
lucasmarquesb <lucasm.marques100@gmail.com>
2023-11-14 10:44:44 -0300

    novos ex

---
+++ aula 15/ambiente.js
@@ -0,0 +1,8 @@
+var num = [5, 2, 8, 9, 3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(num[1])
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
